8b31cf70afa1df0b3a8811603311e49f726016cb
Voidshrey <shreyasg136@gmail.com>
2025-02-14 11:03:19 +0530

    updated file name

--- backend/controller/mealsController.js
+++ backend/controller/mealsController.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
-import User from '../models/User.js';
-import Meal from '../models/meal.js';
-
+import axios from "axios";
+import User from "../models/User.js";
+import Meal from "../models/meal.js";
 
 // Get a random meal
 const getRandomMeal = async (req, res, next) => {
   try {
-    const response = await axios.get("https://www.themealdb.com/api/json/v1/1/random.php");
+    const response = await axios.get(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
     const mealData = response.data.meals[0];
 
     // Check if the meal already exists in the database
     let meal = await Meal.findOne({ mealId: mealData.idMeal });
 
-    
     if (!meal) {
       // Create a new meal document
       meal = new Meal({
@@ -27,13 +27,13 @@ const getRandomMeal = async (req, res, next) => {
       // Add ingredients to the meal document
       for (let i = 1; i <= 20; i++) {
         if (mealData[`strIngredient${i}`]) {
-            if(mealData[`strMeasure${i}`] !== ""){
-          meal.ingredients.push({
-            ingredient: mealData[`strIngredient${i}`]
-          });
+          if (mealData[`strMeasure${i}`] !== "") {
+            meal.ingredients.push({
+              ingredient: mealData[`strIngredient${i}`],
+            });
+          }
         }
       }
-    }
 
       // Save the meal to the database
       await meal.save();
@@ -41,63 +41,64 @@ const getRandomMeal = async (req, res, next) => {
 
     res.json(meal);
   } catch (error) {
-     next(error.message);
+    next(error.message);
   }
 };
 
 const addFavoriteMeal = async (req, res, next) => {
-    try {
-      const { id, mealId } = req.body;
-  
-      const user = await User.findById(id);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      if (!user.favoriteMeals.includes(mealId)) {
-        user.favoriteMeals.push(mealId);
-        await user.save();
-      }
-  
-      res.status(200).json({ message: 'Meal added to favorites' });
-    } catch (error) {
-      next(error);
+  try {
+    const { id, mealId } = req.body;
+
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  };
 
+    if (!user.favoriteMeals.includes(mealId)) {
+      user.favoriteMeals.push(mealId);
+      await user.save();
+    }
 
-  const removeFavoriteMeal = async (req, res, next) => {
-    try {
-      const { id, mealId } = req.body;
-  
-      const user = await User.findById(id);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      if (user.favoriteMeals.includes(mealId)) {
+    res.status(200).json({ message: "Meal added to favorites" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const removeFavoriteMeal = async (req, res, next) => {
+  try {
+    const { id, mealId } = req.body;
+
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.favoriteMeals.includes(mealId)) {
       let index = user.favoriteMeals.indexOf(mealId);
-      if(index > -1){
-        user.favoriteMeals.splice(index , 1);  // index of eleme and delete 1 from that 
+      if (index > -1) {
+        user.favoriteMeals.splice(index, 1); // index of eleme and delete 1 from that
         await user.save();
       }
-      }else{
-       return res.status(404).json({ message: 'Meal not found to remove' });
-      }
-  
-      res.status(200).json({ message: 'Meal removed from favorites successfully' });
-    } catch (error) {
-      next(error);
+    } else {
+      return res.status(404).json({ message: "Meal not found to remove" });
     }
-  };
 
-  const getFavoriteMeals = async (req, res, next) => {
+    res
+      .status(200)
+      .json({ message: "Meal removed from favorites successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getFavoriteMeals = async (req, res, next) => {
   try {
     const { userId } = req.params;
 
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     const favoriteMeals = [];
@@ -106,7 +107,9 @@ const addFavoriteMeal = async (req, res, next) => {
       let meal = await Meal.findOne({ mealId });
       if (!meal) {
         // Fetch meal details from external API
-        const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+        const response = await axios.get(
+          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+        );
         const mealData = response.data.meals[0];
 
         // Create a new meal document
@@ -124,7 +127,7 @@ const addFavoriteMeal = async (req, res, next) => {
           if (mealData[`strIngredient${i}`]) {
             meal.ingredients.push({
               ingredient: mealData[`strIngredient${i}`],
-              measure: mealData[`strMeasure${i}`] || '',
+              measure: mealData[`strMeasure${i}`] || "",
             });
           }
         }
@@ -149,5 +152,4 @@ const addFavoriteMeal = async (req, res, next) => {
   }
 };
 
-
-export { getRandomMeal , addFavoriteMeal , getFavoriteMeals , removeFavoriteMeal};
+export { getRandomMeal, addFavoriteMeal, getFavoriteMeals, removeFavoriteMeal };
